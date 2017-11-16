40b38d87dfb40453a8aec6e677429ba2687785c5
mandeep <mandeepsingh5800.ms@gmail.com>
2017-11-16 13:02:15 +0530

    ui design with user image upload

---
+++ app/src/main/java/com/dupleit/demo/commentmodule/profileDetail.java
@@ -0,0 +1,13 @@
+package com.dupleit.demo.commentmodule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class profileDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_detail);
+    }
+}
